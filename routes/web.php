a4168ccadeb7dcbc11ffe86a1c11ee388516988b
Nguyễn Trung Đức <ducnt97@apecsoft.asia>
2020-10-31 17:49:04 +0700

    Add one-to-one relationship

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,35 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*One to one relationship eloquent laravel */
+Route::get("create-user", function(){
+	$user = new App\User();
+	$user->name = 'nguyentrungduc';
+	$user->password = Hash::make('12345678');
+	$user->email = 'ducnt220997@gmail.com';
+	$user->save();
+
+	return $user->save();
+});
+
+	//Create a phone by user Model
+	Route::get("create-phone-by-user", function(){
+		$user = App\User::find(1);
+
+		$phone = new App\Phone();
+		$phone->phone = '9429343852';
+		 
+		return $user->phone()->save($phone);
+	});
+
+	//Get phone per user
+	Route::get("get-user-phone", function(){
+		$user = App\User::find(1);
+
+		dd([
+			$user->phone(),
+			$user->phone,
+		]);
+	});
+/*One to one relationship eloquent laravel */
